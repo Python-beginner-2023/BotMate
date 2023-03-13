296cf2c140e1a9191f2b4feb8612553ebbd00e21
thiswind <23764+thiswind@users.noreply.github.com>
2023-03-13 16:59:08 +0000

    UPDATE：优化UI，增加代码高亮

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
-
-import os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+
 message_list = [{"role": "system", "content": "You are a helpful assistant."}]
 chat_history = []
+current_token_count = 0
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("chat.html.jinja2", chat_history=chat_history)
+    return render_template("chat.html.jinja2", chat_history=chat_history, current_token_count=current_token_count)
+
 
 @app.route('/forget', methods=['POST'])
 def forget():
-    message_list = [{"role": "system", "content": "You are a helpful assistant."}]
-    chat_history = []
-    return render_template("chat.html.jinja2", chat_history=chat_history)
+    global message_list
+    global chat_history
+    global current_token_count
+
+    message_list.clear()
+    message_list.append({
+        "role": "system",
+        "content": "You are a helpful assistant."
+    })
+    chat_history.clear()
+
+    current_token_count = 0
+    
+    return redirect(url_for("index"))
 
 
 @app.route("/chat", methods=["POST"])
@@ -39,12 +50,16 @@ def chat():
     )
 
     # 从 OpenAI API 的响应中提取回复内容
-    reply = completion.choices[0]['message']['content'] # type: ignore
+    reply = completion.choices[0]['message']['content']  # type: ignore
 
     message_list.append({"role": "assistant", "content": reply})
 
     # 将回复添加到聊天历史中
     chat_history.append((message, reply))
+
+    global current_token_count
+    current_token_count = completion['usage']['total_tokens']
+
     return redirect(url_for("index"))
 
 
