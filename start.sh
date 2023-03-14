914d935377080bc95f199833c89b1ea8cdffe0fa
thiswind <thiswind@gmail.com>
2023-03-14 06:42:22 +0000

    UPDATE: 暂时去掉doppler，直接在web页面打开5000端口

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
-curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
-echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
-sudo apt-get update && sudo apt-get install doppler
+# sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+# curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
+# echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
+# sudo apt-get update && sudo apt-get install doppler
 
 python -m pip install openai
 python -m pip install flask
